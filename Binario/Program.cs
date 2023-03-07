a0486388668f7d8e77711475b7ec74102dcf2603
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-06 22:57:44 -0300

    Add project files.

---
+++ Binario/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int valor, quociente;
+        int resto;
+        int[] vetor = new int[8];
+
+        Console.WriteLine("Digite o valor que será convertido: ");
+        valor = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i <=7; i++)
+        {
+            resto = valor % 2;
+            quociente = valor / 2;
+            valor = quociente;
+            vetor[i] = resto;
+        }
+        for (int i = 7; i >= 0; i--)
+        {
+            Console.Write(vetor[i]);
+        }
+    }
+}
