db71d6f761ef9875f7d815d67390bf4afc57eea9
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-06 23:32:53 -0300

    Conversor de inteiro para binario com recursividade

--- Binario/Program.cs
+++ Binario/Program.cs
@@ -1,25 +1,26 @@
-﻿internal class Program
+﻿int valor,resto, quociente, cont = 7;
+int[] vetor = new int[8];   
+
+Console.WriteLine("Digite o valor a ser convertido: ");
+valor = int.Parse(Console.ReadLine());
+conversor(valor);
+
+int[] conversor(int valor)
 {
-    private static void Main(string[] args)
+    resto = valor % 2;
+    quociente = valor / 2;
+    valor = quociente;
+    vetor[cont] = resto;
+    cont--;
+    if(cont >= 0)
     {
-        int valor, quociente;
-        int resto;
-        int[] vetor = new int[8];
-
-        Console.WriteLine("Digite o valor que será convertido: ");
-        valor = int.Parse(Console.ReadLine());
-
-
-        for (int i = 0; i <=7; i++)
-        {
-            resto = valor % 2;
-            quociente = valor / 2;
-            valor = quociente;
-            vetor[i] = resto;
-        }
-        for (int i = 7; i >= 0; i--)
-        {
-            Console.Write(vetor[i]);
-        }
+        return conversor(valor);
     }
+    return vetor;
+}
+
+
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(vetor[i] + " ");
 }
